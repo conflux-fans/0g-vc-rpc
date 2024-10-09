417d09bb9e8b63fbc826255d17fa8ec6acc5d53a
Pana <pana.wang@outlook.com>
2024-10-09 15:27:21 +0800

    add multi method to stop the server

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,29 @@ use jsonrpsee::server::Server;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     println!("Loading circom, will take a while...");
-    
+
     let circom = circom_builder(&"output".into(), "check_vc");
     let pk = load_proving_key::<false>(&"output".into(), "check_vc")
         .expect("ProvingKey should load success");
 
+    // spawn a thread to handle proof generation
     let (tx, rx) = mpsc::channel::<(VcProveInput, mpsc::Sender<Proof<Bn254>>)>();
-    thread::spawn(move || loop {
-        match rx.recv() {
-            Ok((input, sender)) => {
-                let proof = prove(&pk, &circom, input).expect("generate proof should success"); // TODO handle error
-                sender.send(proof).expect("channel send should success"); // TODO handle error
+    thread::spawn(move || {
+        loop {
+            match rx.recv() {
+                Ok((input, sender)) => {
+                    let proof = prove(&pk, &circom, input).expect("generate proof should success"); // TODO handle error
+                    sender.send(proof).expect("channel send should success"); // TODO handle error
+                }
+                Err(e) => {
+                    println!("Channel Recv Error: {:?}", e);
+                    break;
+                }
             }
-            Err(e) => println!("Channel Recv Error: {:?}", e),
         }
     });
 
+    // start server
     let server = Server::builder()
         .build("127.0.0.1:3030".parse::<SocketAddr>()?)
         .await?;
@@ -42,12 +49,53 @@ async fn main() -> anyhow::Result<()> {
     let handle = server.start(RpcImpl::new(tx).into_rpc());
     println!("Server is listening on: {}", addr);
 
+    // style 1
     // In this example we don't care about doing shutdown so let's it run forever.
     // You may use the `ServerHandle` to shut it down or manage it yourself.
     // EG: monitor the exit signal and shutdown the server
-    handle.stopped().await;
+    // handle.stopped().await;
 
-    // tokio::spawn(handle.stopped());  // spawn a thread to wait for stopped
+    // style 2
+    // Set up a signal handler for graceful shutdown
+    // let (shutdown_sender, shutdown_receiver) = tokio::sync::oneshot::channel();
+
+    // // Spawn a task to handle the shutdown signal
+    // tokio::spawn(async move {
+    //     tokio::signal::ctrl_c().await.expect("Failed to listen for ctrl+c");
+    //     println!("Received shutdown signal. Shutting down...");
+    //     shutdown_sender.send(()).expect("Failed to send shutdown signal");
+    // });
+
+    // // Wait for either the server to stop or the shutdown signal
+    // tokio::select! {
+    //     _ = handle.clone().stopped() => {
+    //         println!("Server stopped unexpectedly");
+    //     }
+    //     _ = shutdown_receiver => {
+    //         println!("Initiating graceful shutdown");
+    //         handle.stop()?;
+    //         println!("Server shut down successfully");
+    //     }
+    // }
+
+    // style 3
+    // tokio::signal::ctrl_c()
+    //     .await
+    //     .expect("Failed to listen for ctrl+c");
+    // handle.stop()?;
+
+    // style 4
+    let handle_clone = handle.clone();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to listen for ctrl+c");
+        // TODO graceful handle unfinished task
+        handle_clone.stop().expect("Failed to stop server");
+    });
+
+    handle.stopped().await;
+    println!("Server stopped");
 
     Ok(())
 }
