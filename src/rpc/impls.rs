424c67030663363a8d25d1f8b19bd4962ad0fd95
Pana <pana.wang@outlook.com>
2024-11-21 19:05:57 +0800

    add log

--- src/rpc/impls.rs
+++ src/rpc/impls.rs
@@ -3,6 +3,8 @@ use ark_groth16::PreparedVerifyingKey;
 use ark_groth16::Proof;
 use ark_serialize::CanonicalDeserialize;
 use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::sync::mpsc;
 
 use crate::rpc::api::ZgVcServer;
@@ -36,6 +38,11 @@ impl RpcImpl {
 #[async_trait]
 impl ZgVcServer for RpcImpl {
     async fn generate_proof(&self, input: VcProveInput) -> Result<VcProof, ErrorObjectOwned> {
+        write_log(&format!(
+            "generate_proof: {}\n",
+            serde_json::to_string(&input).unwrap()
+        ));
+
         let (tx, rx) = mpsc::channel::<Proof<Bn254>>();
         self.sender
             .send((input, tx))
@@ -43,7 +50,14 @@ impl ZgVcServer for RpcImpl {
         let proof = rx
             .recv()
             .map_err(|e| jsonrpc_message_error(e.to_string()))?;
-        Ok(VcProof(proof))
+        let res = VcProof(proof);
+
+        write_log(&format!(
+            "generate_proof result: {}\n",
+            serde_json::to_string(&res).unwrap()
+        ));
+
+        Ok(res)
     }
 
     async fn verify_proof(
@@ -51,8 +65,15 @@ impl ZgVcServer for RpcImpl {
         proof: VcProof,
         public_inputs: VcVerifyInput,
     ) -> Result<bool, ErrorObjectOwned> {
+        write_log(&format!(
+            "verify_proof: proof {} public_inputs {}\n",
+            serde_json::to_string(&proof).unwrap(),
+            serde_json::to_string(&public_inputs).unwrap()
+        ));
         let result = verify(&self.vk, &proof.0, &public_inputs)
             .map_err(|e| jsonrpc_message_error(e.to_string()))?;
+
+        write_log(&format!("verify_proof result: {}\n", result));
         Ok(result)
     }
 
@@ -63,4 +84,15 @@ impl ZgVcServer for RpcImpl {
 
 pub fn jsonrpc_message_error(msg: String) -> ErrorObjectOwned {
     ErrorObjectOwned::owned(INTERNAL_ERROR_CODE, msg, Some(""))
+}
+
+fn write_log(line: &str) {
+    // Open the file in append mode
+    let mut file = OpenOptions::new()
+        .create(true) // Create the file if it doesn't exist
+        .append(true) // Open in append mode
+        .open("nohup.log")
+        .expect("Unable to open file");
+    file.write_all(line.as_bytes())
+        .expect("write log should success");
 }
